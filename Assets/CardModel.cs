b3f7215baf79afcf7491922d1fa3d7ad9f0608bd
Stomoarch <36717488+Stomoarch@users.noreply.github.com>
2019-02-22 05:01:33 +0900

    new backgrouond and cards
    adding background and new cards

--- Assets/CardModel.cs
+++ Assets/CardModel.cs
@@ -10,7 +10,8 @@ public class CardModel : MonoBehaviour
     // SpriteRendererクラスを参照します。①（Cardmodelクラスが直接アタッチされているオブジェクトのクラスを参照void awakeの所とセット）
 
     public Sprite[] faces;　　// 外部から参照できる（インスペクターからいじれる）facesという表面のリストの宣言。
-    public Sprite cardBack;　// 外部から参照できる（インスペクターからいじれる）cardBackという裏面の宣言。
+    public Sprite cardBack;
+    public Sprite cardBack2;// 外部から参照できる（インスペクターからいじれる）cardBackという裏面の宣言。
 
     public int cardIndex;　// 外部から参照できるcardIndex という値の宣言。
 
@@ -23,7 +24,15 @@ public class CardModel : MonoBehaviour
         }
         else
         {
-            spriteRenderer.sprite = cardBack;　　//②で取得したspriteにcardbackをレンダーさせる。
+            if(cardIndex == 8 || cardIndex == 9 || cardIndex == 12 || cardIndex == 13 || cardIndex == 14)
+            {
+                spriteRenderer.sprite = cardBack2;
+            }
+            else
+            {
+                spriteRenderer.sprite = cardBack;　　//②で取得したspriteにcardbackをレンダーさせる。
+            }
+           
         }
     }
 
